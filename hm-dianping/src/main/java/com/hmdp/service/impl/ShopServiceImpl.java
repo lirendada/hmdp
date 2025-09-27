7e24894a997c54ae0ebc2990207f36c4d85b3152
lirendada <2916776007@qq.com>
2025-09-27 12:26:28 +0800

    完成一些注释内容

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -33,7 +33,6 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Override
     public Result getShopByID(Long id) {
         Shop shop = cacheClient.getShopPreventBreakDownByLogicExpire(RedisConstants.CACHE_SHOP_KEY, id, Shop.class, this::getById, RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
-
         if(shop == null) {
             return Result.fail("该商铺不存在！");
         }
@@ -41,65 +40,6 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         // 返回
         return Result.ok(shop);
     }
-
-
-    /*
-    // 缓存击穿（互斥锁实现方式，因为没有逻辑过期，所以会过期，请求可能直接打在数据库上，所以需要防止缓存穿透）
-    private Shop getShopByBreakDownMutex(Long id) {
-        // 1. 从redis中查询shop缓存（这里用string类型演示）
-        String cache_key = RedisConstants.CACHE_SHOP_KEY + id;
-        String shopJson = stringRedisTemplate.opsForValue().get(cache_key);
-
-        // 2. 存在缓存，并且有内容，说明是真的有信息，则直接返回
-        if (StringUtils.hasText(shopJson)) {
-            return JSONUtil.toBean(shopJson, Shop.class);
-        }
-
-        // 存在缓存，但是没有内容，说明是空对象，是为了避免缓存穿透的，直接返回null
-        if(shopJson != null) {
-            return null;
-        }
-
-        try {
-            // 3. 不存在缓存，先获取锁
-            boolean isLock = tryLock(id);
-
-            // 3.1 如果拿不到锁，休眠后重试
-            if (!isLock) {
-                Thread.sleep(50);
-                return getShopByBreakDownMutex(id); // 递归去调用重试
-            }
-
-            // 3.2 如果拿到锁，进行doublecheck，防止不必要的访问数据库
-            String doublecheck = stringRedisTemplate.opsForValue().get(cache_key);
-            if(StringUtils.hasText(doublecheck)) {
-                return JSONUtil.toBean(doublecheck, Shop.class);
-            }
-
-            // 实在没有再去查数据库
-            Shop shop = getById(id);
-
-            // 3.3 如果数据库中不存在，不是直接返回错误，而是将该id写入redis，设为空对象，设置ttl，避免缓存穿透💥
-            if(shop == null) {
-                stringRedisTemplate.opsForValue().set(
-                        RedisConstants.CACHE_SHOP_KEY + id,
-                        "",
-                        RedisConstants.CACHE_NULL_TTL,
-                        TimeUnit.MINUTES);
-                return null;
-            }
-
-            // 3.4 数据库中存在，将数据写到redis中，设置过期时间（缓存更新策略）
-            stringRedisTemplate.opsForValue().set(cache_key, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
-
-            return shop;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            unlock(id);
-        }
-    }
-    */
 
     @Override
     @Transactional
